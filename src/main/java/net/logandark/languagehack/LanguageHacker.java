c0ab2714326949fb6231c3c7105a13b4ba99a7d5
LoganDark <logandarklock@gmail.com>
2022-03-16 11:17:40 -0700

    update to 1.18, latest fabric, multiple roots

--- src/main/java/net/logandark/languagehack/LanguageHacker.java
+++ src/main/java/net/logandark/languagehack/LanguageHacker.java
@@ -4,9 +4,9 @@ import com.google.common.collect.ImmutableMap;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
 import net.minecraft.util.Language;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.util.Closer;
+import net.minecraft.util.Pair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,52 +22,61 @@ import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class LanguageHacker {
-	private static final Logger LOGGER = LogManager.getLogger("LanguageHack");
+	private static final Logger LOGGER = LoggerFactory.getLogger(LanguageHacker.class);
 	private static final AtomicBoolean ACTIVATED = new AtomicBoolean();
 
 	private static String[] getChain() {
 		return new String[]{"en_us"};
 	}
 
-	private static List<Path> findLangDirs(Path assets) throws IOException {
-		List<Path> lang_dirs = new ArrayList<>();
+	private static List<Pair<Path, Path>> findLangDirs(ModContainer container) throws IOException {
+		List<Pair<Path, Path>> langDirs = new ArrayList<>();
 
-		try (DirectoryStream<Path> stream = Files.newDirectoryStream(assets)) {
-			stream.forEach(path -> {
-				Path lang_path = path.resolve("lang");
+		for (Path root : container.getRootPaths()) {
+			Path assetsDir = root.resolve("assets");
 
-				if (Files.isDirectory(lang_path)) {
-					lang_dirs.add(lang_path);
+			if (Files.isDirectory(assetsDir)) {
+				try (DirectoryStream<Path> stream = Files.newDirectoryStream(assetsDir)) {
+					stream.forEach(path -> {
+						Path langDir = path.resolve("lang");
+
+						if (Files.isDirectory(langDir)) {
+							langDirs.add(new Pair<>(assetsDir, langDir));
+						}
+					});
 				}
-			});
+			}
 		}
 
-		return lang_dirs;
+		return langDirs;
 	}
 
 	private static Map<String, String> loadTranslations(ModContainer container, String[] chain) {
 		String modid = container.getMetadata().getId();
 		Map<String, String> translations = new HashMap<>();
 
-		List<Path> lang_dirs;
+		List<Pair<Path, Path>> langDirs;
 
 		try {
-			lang_dirs = findLangDirs(container.getPath("assets"));
-			LOGGER.info("{}: Found {} lang directories to check", modid, lang_dirs.size());
+			langDirs = findLangDirs(container);
+			LOGGER.info("{}: Found {} lang directories to check", modid, langDirs.size());
 		} catch (IOException e) {
-			LOGGER.warn("{}: Error scanning lang directories", modid);
+			LOGGER.warn("{}: Error scanning lang directories: {}", modid, e);
 			return translations;
 		}
 
-		for (Path dir : lang_dirs) {
+		for (Pair<Path, Path> dir : langDirs) {
+			Path rootDir = dir.getLeft();
+			Path langDir = dir.getRight();
+
 			InputStream inputStream = null;
 
 			for (String lang : chain) {
-				Path lang_path = dir.resolve(lang + ".json");
+				Path langFile = langDir.resolve(lang + ".json");
 
 				try {
-					inputStream = Files.newInputStream(lang_path);
-					LOGGER.info("{}: Found {} translations ({})", modid, lang, container.getRootPath().relativize(lang_path));
+					inputStream = Files.newInputStream(langFile);
+					LOGGER.info("{}: Found {} translations ({})", modid, lang, rootDir.relativize(langFile));
 					break;
 				} catch (IOException e) {
 					if (!(e instanceof NoSuchFileException)) {
@@ -84,7 +93,11 @@ public class LanguageHacker {
 			try {
 				Language.load(inputStream, translations::put);
 			} finally {
-				Closer.closeSilently(inputStream);
+				try {
+					inputStream.close();
+				} catch (IOException e) {
+					LOGGER.error(e.toString());
+				}
 			}
 		}
 
